b5fa32d337fb51da8d0409df009e1fa091d9fce3
metinfurkankose <metinfurkankose@hotmail.com>
2024-11-20 15:07:31 +0300

    Update MainController.java
    HttpServletrequest ve model kullanımı

--- src/main/java/com/mfk/notalma/MainController.java
+++ src/main/java/com/mfk/notalma/MainController.java
@@ -2,6 +2,7 @@ package com.mfk.notalma;
 
 import java.util.Locale;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController {
     //@RequestMapping("/")
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(Model model) {//Buradaki model inversition of control
+    public String home(Model model, HttpServletRequest req) {//Buradaki model inversition of control
         model.addAttribute("sayi",12);
         model.addAttribute("tempattr","/");
+        System.out.println(req.getRemoteAddr());
         return "index";
     }
     @RequestMapping(value = "/metin", method = RequestMethod.GET)
